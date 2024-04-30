0f74b9ed447e1f999a1412f880c44e6c121d1d98
inde153 <inde153@naver.com>
2024-04-30 15:30:48 +0900

    Fix : main.ts에 Cors옵션 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: '*',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+    maxAge: 81600,
+  };
+
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();

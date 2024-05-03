ea3c58581e511fc16a60ed34475775c8bcee2001
inde153 <inde153@naver.com>
2024-05-04 02:44:12 +0900

    Feat : guees DTO

---
+++ src/records/dtos/get-guess.dto.ts
@@ -0,0 +1,17 @@
+import { CoreOutput } from 'src/common/dtos/output.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Word } from 'src/words/entities/word.entity';
+
+export class GetWordInput {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  categoryID: number;
+}
+
+export class GetWordOutput extends CoreOutput {
+  data: Word | {};
+}

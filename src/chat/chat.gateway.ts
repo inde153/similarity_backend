2d9d2f8d1f98cae5a848d5562fe9abc2c975b209
inde153 <inde153@naver.com>
2024-05-05 03:53:29 +0900

    Feat : 실시간 채팅 모듈 작업

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,7 +1,46 @@
-import { WebSocketGateway } from '@nestjs/websockets';
-import { ChatService } from './chat.service';
+import { Logger } from '@nestjs/common';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  WsResponse,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatGateway {
-  constructor(private readonly chatService: ChatService) {}
+@WebSocketGateway({ namespace: 'socket', cors: true })
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+  logger = new Logger();
+
+  afterInit(server: Server) {
+    this.server = server;
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('message')
+  handleEvent(client: Socket, data: string): WsResponse<string> {
+    const username = 'User'; // Replace with user authentication logic
+    const timestamp = new Date().toLocaleTimeString();
+
+    const response: WsResponse<string> = {
+      event: 'message',
+      data: `${timestamp} - ${username}: ${data}`,
+    };
+
+    this.server.emit('message', response.data); // Broadcast the message to all clients
+    return response;
+  }
 }

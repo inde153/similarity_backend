8f706468888ad9f46c923bd605de4b59d2ab23a7
inde153 <inde153@naver.com>
2024-05-08 08:24:31 +0900

    Fix : Payload 인터페이스 변경

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -1,5 +1,7 @@
+import { UserLoginType } from 'src/users/entities/user.entity';
+
 export interface Payload {
   id: number;
   username: string;
-  loginType: string;
+  loginType: UserLoginType;
 }

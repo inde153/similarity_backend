09bf54fb93f0ee9c5c23561fe47887bfed0e8644
inde153 <inde153@naver.com>
2024-05-07 11:25:21 +0900

    Feat : 코어 아웃풋 인터셉터 구현

---
+++ src/common/interceptors/Interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map, tap } from 'rxjs';
+import { CoreOutput } from '../dtos/output.dto';
+
+@Injectable()
+export class LogginInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Promise<any> | Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const path = request.originalUrl;
+    const logger = new Logger();
+
+    logger.log(`${path}`);
+
+    return next.handle().pipe(
+      tap((data) => console.log(data)),
+      map(
+        (data): CoreOutput => ({
+          success: true,
+          message: '',
+          errorCode: 0,
+          data,
+        }),
+      ),
+    );
+  }
+}

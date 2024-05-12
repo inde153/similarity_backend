08d5ef5306655ddf977ca33e9867966f7c0986b0
inde153 <inde153@naver.com>
2024-05-12 22:08:27 +0900

    Feat : Cookieparser 및 CORS

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,21 @@ import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { APIInterceptor } from './common/interceptors/api.interceptor';
 import { HttpExceptionFilter } from './common/exception/exception.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = 8080;
   const corsOptions: CorsOptions = {
-    origin: '*',
-    methods: '*',
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT',
+    allowedHeaders: 'Origin,Content-Type,Authorization,Accept',
     credentials: true,
     maxAge: 81600,
   };
-
   app.enableCors(corsOptions);
 
+  app.use(cookieParser());
   app.useGlobalInterceptors(new APIInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
 

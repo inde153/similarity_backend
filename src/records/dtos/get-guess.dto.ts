116ee7bfa85c760f4f735e1877f37c66081844a1
inde153 <inde153@naver.com>
2024-05-15 14:02:51 +0900

    Refactor : DTO 설명추가

--- src/records/dtos/get-guess.dto.ts
+++ src/records/dtos/get-guess.dto.ts
@@ -5,9 +5,11 @@ import { ApiProperty } from '@nestjs/swagger';
 export class WordInputDTO {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ description: '추측 단어' })
   name: string;
 
   @IsString()
+  @ApiProperty({ description: '카테고리 번호' })
   categoryID: number;
 }
 
